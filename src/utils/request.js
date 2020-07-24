6dfd83daf04bb87643774a3a7636a31849a0e766
Cong Do <dochauthanhcong147@gmail.com>
2020-07-25 03:18:15 +0700

    - Update header Axios instance

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,10 @@ import { getToken } from '@/utils/auth'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  timeout: 5000, // request timeout
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
 // request interceptor
@@ -19,7 +22,7 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers['Authorization'] = 'Bearer ' + getToken()
     }
     return config
   },
